5c7c745434f0a94057e5a50dc7321118ad815c94
Kimhong0710 <namcungvian1234@gmail.com>
2025-02-26 10:04:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ funtion login()
 {
   let u = username.value
   let p = password.value
-  If 
+  If (u=="hong" && p=="kimhong0710")
+  {
+    window.open(" home.html ")
+    document.getElementbyId(compiler).style.display=" block "
